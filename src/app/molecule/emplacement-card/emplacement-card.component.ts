b475099d767c414315affb29ad66cb2aa96a3b2f
Vincent Muffato <vincent.muffato@atos.net>
2024-12-07 14:14:32 +0100

    add emplacement card

--- src/app/molecule/emplacement-card/emplacement-card.component.ts
+++ src/app/molecule/emplacement-card/emplacement-card.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
-import { IonIcon } from "@ionic/angular/standalone";
+import { Router, RouterLink } from '@angular/router';
+import { IonIcon, IonButton, IonText } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'emplacement-card',
   standalone: true,
-  imports: [IonIcon],
+  imports: [IonText, IonButton, IonIcon, RouterLink],
   templateUrl: './emplacement-card.component.html',
   styleUrl: './emplacement-card.component.scss',
 })
 export class EmplacementCardComponent {
-
   @Input() name: string = "Test";
   @Input() productAmount: number = 0;
+  @Input() contentPath: string = 'content'; // must redirect following content ID
 }
 
 
