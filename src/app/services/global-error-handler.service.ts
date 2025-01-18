28eacefaa22b24cfe47228ec2263dbe3fdae9ac1
Vincent Muffato <vincent.muffato@atos.net>
2025-01-18 14:05:46 +0100

    Commenting notification service (cause use Material angular and can be replace by ionic-toaster)

--- src/app/services/global-error-handler.service.ts
+++ src/app/services/global-error-handler.service.ts
@@ -1,14 +1,15 @@
 import { HttpErrorResponse } from "@angular/common/http"
 import { Injectable } from "@angular/core"
 import { Router } from "@angular/router"
-import { Toaster } from "@app/utils/toaster"
+// import { Toaster } from "@app/utils/toaster"
+import { ExceptionCode } from "@capacitor/core"
 
 @Injectable({
     providedIn: "root",
 })
 export class ErrorHandlingService {
     constructor(
-        private toaster: Toaster,
+        // private toaster: Toaster,
         private router: Router
     ) {}
 
@@ -19,40 +20,64 @@ export class ErrorHandlingService {
 
         switch (status) {
             case 400:
-                this.toaster.error("Requête incorrecte. Vérifiez les données.")
+                //this.toaster.error("Requête incorrecte. Vérifiez les données.")
+                throw new HttpErrorResponse({ status: 400 })
                 break
 
             case 401:
-                this.toaster.error("Veuillez vous reconnecter.")
+                //this.toaster.error("Veuillez vous reconnecter.")
+                throw new HttpErrorResponse({ status: 401 })
                 this.router.navigate(["/login"])
                 break
 
             case 403:
-                this.toaster.error(
-                    "Accès refusé. Vous n'avez pas les permissions nécessaires."
-                )
+                throw new HttpErrorResponse({
+                    status: 403,
+                    statusText:
+                        "Accès refusé. Vous n'avez pas les permissions nécessaires.",
+                })
+                // this.toaster.error(
+                //     "Accès refusé. Vous n'avez pas les permissions nécessaires."
+                // )
                 break
 
             case 409:
-                this.toaster.error(
-                    "Conflit lors de l'opération. Vérifiez les données envoyées."
-                )
+                throw new HttpErrorResponse({
+                    status: 409,
+                    statusText:
+                        "Conflit lors de l'opération. Vérifiez les données envoyées.",
+                })
+                // this.toaster.error(
+                //   "Conflit lors de l'opération. Vérifiez les données envoyées."
+                // )
                 break
 
             case 500:
-                this.toaster.error(
-                    "Erreur interne du serveur. Merci de réessayer plus tard."
-                )
+                throw new HttpErrorResponse({
+                    status: 500,
+                    statusText:
+                        "Erreur interne du serveur. Merci de réessayer plus tard.",
+                })
+
+                // this.toaster.error(
+                //     "Erreur interne du serveur. Merci de réessayer plus tard."
+                // )
                 break
 
             case 503:
-                this.toaster.error(
-                    "Service indisponible. Merci de réessayer plus tard."
-                )
+                throw new HttpErrorResponse({
+                    status: 503,
+                    statusText:
+                        "Service indisponible. Merci de réessayer plus tard.",
+                })
+                // this.toaster.error(
+                //     "Service indisponible. Merci de réessayer plus tard."
+                // )
                 break
 
             default:
-                this.toaster.error(message)
+                throw new HttpErrorResponse({ statusText: message })
+                // this.toaster.error(message)
                 break
         }
     }
