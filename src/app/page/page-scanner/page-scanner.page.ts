11b216e7f739ec07dfc6e97f9332de140e6d405e
Vincent Muffato <vincent.muffato@atos.net>
2025-02-15 10:24:42 +0100

    PageScanner - Remove addProduct modal (useless)

--- src/app/page/page-scanner/page-scanner.page.ts
+++ src/app/page/page-scanner/page-scanner.page.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from "@angular/common"
 import { Component } from "@angular/core"
 import { IonicModule } from "@ionic/angular"
-import { AddProductModalComponent } from "../../molecule/add-product-modal/add-product-modal.component"
+import { BarcodeEntryModalComponent } from "../../molecule/barcode-entry-modal/barcode-entry-modal.component"
 
 @Component({
     selector: "app-page-scanner",
     templateUrl: "./page-scanner.page.html",
     styleUrls: ["./page-scanner.page.scss"],
     standalone: true,
-    imports: [IonicModule, CommonModule, AddProductModalComponent],
+    imports: [IonicModule, CommonModule, BarcodeEntryModalComponent],
 })
 export class PageScannerPage {
     isModalOpen: boolean = false
