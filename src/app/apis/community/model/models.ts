e99e39912f2808d2634a7599aa6613617e6808ce
Regitraz <48715468+regitraz2@users.noreply.github.com>
2025-03-01 15:42:49 +0100

    chore(apis): added community api

---
+++ src/app/apis/community/model/models.ts
@@ -0,0 +1,7 @@
+export * from './city';
+export * from './community';
+export * from './communityResponse';
+export * from './invitationCreationRequest';
+export * from './invitationEntity';
+export * from './roleEntityDto';
+export * from './userEntityDto';
