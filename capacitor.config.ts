39f810b1291896dc7de0f29a474d3beae88cfb68
JReb20 <julien.rebiffe@gmail.com>
2025-03-08 14:47:28 +0100

    change capacitor config for scan

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
-import type { CapacitorConfig } from "@capacitor/cli"
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: "io.ionic.starter",
-    appName: "Frontend",
-    webDir: "www",
-}
+    appId: 'io.ionic.starter',
+    appName: 'product-scanner',
+    webDir: 'www',
+    server: {
+        androidScheme: 'https'
+    },
+    plugins: {
+        CapacitorHttp: {
+            enabled: true
+        }
+    }
+};
 
-export default config
+
+export default config;
